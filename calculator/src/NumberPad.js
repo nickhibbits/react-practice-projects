317b6fa095ef619c6b2d37128a516a0032f8b7b3
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-08-14 16:06:59 -0700

    create num pad grid

--- calculator/src/NumberPad.js
+++ calculator/src/NumberPad.js
@@ -1,9 +1,41 @@
+import "../src/NumberPad.css";
 function NumberPad() {
+  const group1 = [1, 2, 3];
+  const group2 = [4, 5, 6];
+  const group3 = [7, 8, 9];
+
   return (
     <div className="number-pad-component">
-      {[...Array(10)].map((_, i) => {
+      {/* {[...Array(10)].map((_, i) => {
         return <div className="number-button">{i}</div>;
-      })}
+      })} */}
+      <div className="num-group" style={{ gridArea: "1 / 1 / 1 / 3" }}>
+        {group1.map((num) => {
+          return (
+            <div key={num} className="num-button">
+              {num}
+            </div>
+          );
+        })}
+      </div>
+      <div className="num-group" style={{ gridArea: "1 / 1 / 1 / 3" }}>
+        {group2.map((num) => {
+          return (
+            <div key={num} className="num-button">
+              {num}
+            </div>
+          );
+        })}
+      </div>
+      <div className="num-group" style={{ gridArea: "1 / 1 / 1 / 3" }}>
+        {group3.map((num) => {
+          return (
+            <div key={num} className="num-button">
+              {num}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
